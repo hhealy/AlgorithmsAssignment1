c3de41a73eba56454ca11d0f4a4d555773217550
Henry Healy <henryjdhealy@gmail.com>
2016-11-04 13:09:00 +0000

    Added working BruteAutoComplete and BruteAutoCompleteTest

--- src/assignment1/BruteAutoComplete.java
+++ src/assignment1/BruteAutoComplete.java
@@ -1,25 +1,68 @@
 package assignment1;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.io.IOException;
 
 public class BruteAutoComplete implements Autocomplete 
 {
+	ReadTerms terms;
+	private ArrayList<String> result;
+	
+	public BruteAutoComplete() throws IOException
+	{
+		terms = new ReadTerms();
+	}
+	
 	@Override
-	public double weightOf(String term) {
-		// TODO Auto-generated method stub
+	public double weightOf(String term) 
+	{
+		if (term != null)
+		{
+			for (int i = 0; i < terms.terms.size(); i++)
+			{
+				if (term.equals(terms.terms.get(i).getTerm()))
+					return terms.terms.get(i).getWeight();
+			}
+		}
 		return 0;
 	}
 
 	@Override
-	public String bestMatch(String prefix) {
-		// TODO Auto-generated method stub
+	public String bestMatch(String prefix)
+	{
+		if (prefix != null)
+		{
+			for (int i = 0; i < terms.terms.size(); i++)
+			{
+				String comp = terms.terms.get(i).getTerm();
+				if (comp.startsWith(prefix))
+					return comp;
+			}
+		}
 		return null;
 	}
 
 	@Override
-	public Iterable<String> matches(String prefix, int k) {
-		// TODO Auto-generated method stub
-		return null;
+	public Iterable<String> matches(String prefix, int k) 
+	{
+		result = new ArrayList<String>();
+		
+		if (prefix == null)
+			return null;
+		
+		if (k < terms.terms.size())
+		{
+			for (int i = 0; result.size()<k && i<terms.terms.size(); i++)
+			{
+				String comp = terms.terms.get(i).getTerm();
+				if (comp.startsWith(prefix) && result.size() < k)
+					result.add(comp);
+			}
+		}
+		
+		if (result.size() == 0)
+			return null;
+		
+		return result;
 	}
-
 }
---
+++ test/assignment1/BruteAutoCompleteTest.java
@@ -0,0 +1,95 @@
+package assignment1;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class BruteAutoCompleteTest 
+{
+	private BruteAutoComplete bruteAuto;
+	private Term term5;
+	private Term term35;
+	private Term term100;
+	private Term term156;
+	private Term term7700;
+	private ReadTerms terms;
+	private ArrayList<String> results1;
+	private ArrayList<String> results2;
+	
+	@Before
+	public void setUp() throws IOException
+	{
+		terms = new ReadTerms();
+		bruteAuto = new BruteAutoComplete();
+		term5 = new Term(1742063600, "in");
+		term35 = new Term(327769900, "my");
+		term100 = new Term(92305300, "men");
+		term156 = new Term(56467400, "get");
+		term7700 = new Term(681404, "particles");
+		
+		results1 = new ArrayList<String>();
+		results1.add(terms.terms.get(4).getTerm()); //adds the word "in"
+		results1.add(terms.terms.get(55).getTerm()); //adds the word "into"
+		results1.add(terms.terms.get(329).getTerm()); //add the word "indeed"
+		
+		results2 = new ArrayList<String>();
+		results2.add(terms.terms.get(2).getTerm()); //adds the word "and"
+		results2.add(terms.terms.get(40).getTerm()); //adds the word "an"
+		results2.add(terms.terms.get(57).getTerm()); //adds the word "any"
+		results2.add(terms.terms.get(165).getTerm()); //adds the word "another"
+		results2.add(terms.terms.get(292).getTerm()); //adds the word "anything"
+		results2.add(terms.terms.get(340).getTerm()); //adds the word "answered"
+	}
+	
+	@Test
+	public void testWeightOf() 
+	{
+		assertEquals(1742063600, bruteAuto.weightOf("in"), 0.01);
+		assertEquals(327769900, bruteAuto.weightOf("my"), 0.01);
+		assertEquals(92305300, bruteAuto.weightOf("men"), 0.01);
+		assertEquals(56467400, bruteAuto.weightOf("get"), 0.01);
+		assertEquals(681404, bruteAuto.weightOf("particles"), 0.01);
+	}
+	
+	@Test
+	public void testFailedWeightOf()
+	{
+		assertEquals(0, bruteAuto.weightOf("FailureTest"), 0.01);
+		assertEquals(0, bruteAuto.weightOf(""), 0.01);
+	}
+	
+	@Test
+	public void testBestMatch()
+	{
+		assertEquals(term5.getTerm(), bruteAuto.bestMatch("in"));
+		assertEquals(term35.getTerm(), bruteAuto.bestMatch("my"));
+		assertEquals(term100.getTerm(), bruteAuto.bestMatch("men"));
+		assertEquals(term156.getTerm(), bruteAuto.bestMatch("get"));
+		assertEquals(term7700.getTerm(), bruteAuto.bestMatch("particles"));
+	}
+	
+	@Test
+	public void testFailedBestMatch()
+	{
+		assertEquals(null, bruteAuto.bestMatch(null));
+		assertEquals(null, bruteAuto.bestMatch("FailureTest"));
+	}
+	
+	@Test
+	public void testMatches()
+	{
+		assertEquals(results1, bruteAuto.matches("in", 3));
+		assertEquals(results2, bruteAuto.matches("an", 6));
+	}
+	
+	@Test
+	public void testFailedMatches()
+	{
+		assertEquals(null, bruteAuto.matches(null, 5));
+		assertEquals(null, bruteAuto.matches("FailureTest", 20000));
+		assertEquals(null, bruteAuto.matches(null, 30000));
+		assertEquals(null, bruteAuto.matches("in", -4));
+	}
+}
